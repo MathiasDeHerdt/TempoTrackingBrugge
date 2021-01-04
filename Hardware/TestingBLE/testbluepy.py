13e61b0dad6dcf1022451948446b026587584012
Bent Roggeman <bent.roggeman@student.howest.be>
2021-01-04 16:24:38 +0100

    Raspberry Pi Distance

--- Hardware/TestingBLE/testbluepy.py
+++ Hardware/TestingBLE/testbluepy.py
@@ -1,12 +1,11 @@
 from bluepy import btle
-test_pheri = "d9:aa:73:97:3b:0a"
-test_uuid = "e9dab8c2-403d-416e-8029-7ebf83b01681"
+import math
 txPower = -65.0
 
 
 def scan_for_devices():
     scanner = btle.Scanner()
-    devices = scanner.scan(10)
+    devices = scanner.scan(0.5)
     for device in devices:
         dev_addr = str(device.addr)
         dev_rssi = str(device.rssi)
@@ -15,19 +14,12 @@ def scan_for_devices():
         print(("Distance = {} m").format(distance))
 
 def calculateAccuracy(txPower, rssi):
-    if rssi == 0:
-        return -1.0; #if we cannot determine accuracy, return -1.
-
-    ratio = rssi * 1.0 / txPower
-    if ratio < 1.0:
-        return ratio **10
-
-    else:
-        accuracy =  0.89976 * (ratio ** 7.7095) + 0.111;    
-        return accuracy
+    distance = math.pow(10, ((txPower - rssi) / (10 * 2)))
+    return distance
 
 try:
-    scan_for_devices()
+    while True:
+        scan_for_devices()
     print("finish program")
 except Exception as e:
   print("An exception occurred") 
