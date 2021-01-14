eb54394e12f66ce7fd5618f333577575f02ce3d2
mathias <mathias.de.herdt@student.howest.be>
2021-01-14 12:09:56 +0100

    added sidebar.js

--- Frontend/Script/appMP.js
+++ Frontend/Script/appMP.js
@@ -1,19 +1,6 @@
 // #region ==== DOM //
 document.addEventListener('DOMContentLoaded', function() {
-    console.log("DOM - Main Page")
-
-    // #region ===== Sidebar  //
-
-    // width moet nog aangepast worden naar transform.
-    document.getElementById("js-button-open").addEventListener("click",function(){
-    document.getElementById("myNav").style.transform= "translateX(0)";
-    });
-
-    document.getElementById("js-option-close").addEventListener("click",function(){
-    document.getElementById("myNav").style.transform= "translateX(-100%)";
-    });
-    // #endregion
-    
+    console.log("DOM - Main Page")  
 
     // #region ===== timer  //
 
